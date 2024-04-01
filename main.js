96461253d7e8d502e801e95b1f91add16c18f60b
Sakshi Maheshwari <sakshicoder800@gmail.com>
2024-04-01 16:53:45 +0530

    .

--- main.js
+++ main.js
@@ -11,3 +11,13 @@ e.addEventListener("keyup",(event)=>{
         document.getElementsByClassName("fa-circle-chevron-up")[i].style.transition = ".2s linear"
     }
 })})
+
+
+const mobile_btn = document.getElementsByClassName("mobile-btn")[0];
+
+mobile_btn.addEventListener("click", () => {
+    const menu_bar = document.getElementsByClassName("right-menu")[0];
+    menu_bar.classList.toggle("ul_active");
+});
+
+// .ul_active
